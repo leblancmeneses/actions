32e61d0cb4916efb97d185a1dbd5ba774771700f
Leblanc Meneses <leblancmeneses@gmail.com>
2024-05-28 14:58:22 -0500

    changes

--- apps/version-autopilot/src/main.ts
+++ apps/version-autopilot/src/main.ts
@@ -1,11 +1,33 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+function getVersion(versionMajor, versionMinor, versionPatch, versionShift) {
+  const patch = (versionPatch + versionShift)%100;
+  const minor = (versionMinor + Math.floor((versionPatch + versionShift)/100)) % 100;
+  const major = versionMajor + Math.floor((versionMinor + Math.floor((versionPatch + versionShift)/100))/100);
+  const appVersionString = `${major}.${minor}.${patch}`;
+  const appVersionCode= major*10000 + minor*100 + patch;
+  return {
+    appVersionString,
+    appVersionCode
+  };
+}
 
 async function run() {
   try {
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
-    core.setOutput('time', time);
+    const major = parseInt(core.getInput('major') || '0', 10);
+    const minor = parseInt(core.getInput('minor') || '0', 10);
+    const shift = parseInt(core.getInput('shift') || '0', 10);
+
+    const patch = core.getInput('patch')? parseInt(core.getInput('patch') || '0', 10) : github.context.runNumber;
+    const version = getVersion(major, minor, patch, shift);
+    console.log(`version: ${JSON.stringify(version, null, 2)}!`);
+    const shortSha = `${github.context.sha}`.substring(0, 12);
+    if (github.context.eventName === 'pull_request') {
+      core.setOutput('version_autopilot_string_recommended', `${version.appVersionString}-pr-${github.context.payload.pull_request.number}-${shortSha}`);
+    } else {
+      core.setOutput('version_autopilot_string_recommended', `${version.appVersionString}-${github.context.ref}-${shortSha}`);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
