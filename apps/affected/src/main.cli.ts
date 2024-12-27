152ac0f3b65189f2fcecc775659d3621afef9904
Leblanc Meneses <leblancmeneses@gmail.com>
2024-12-27 13:23:38 -0600

    changes

---
+++ apps/affected/src/main.cli.ts
@@ -0,0 +1,46 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { processRules, getRules } from './common';
+
+export const log = (verbose: boolean, message: string) => {
+  if (verbose) {
+    console.log(message);
+  }
+};
+
+yargs(hideBin(process.argv))
+  .command(
+    'run',
+    'Process rules',
+    (yargs) => {
+      yargs
+        .option('rules', { type: 'string', describe: 'Rules as a string', demandOption: false })
+        .option('rules-file', { type: 'string', describe: 'Path to rules file', demandOption: false })
+        .option('verbose', { type: 'boolean', default: false, describe: 'Verbose logging' })
+        .option('truncate-sha1-size', { type: 'number', default: 0, describe: 'SHA1 truncation size' })
+        .option('image-tag-prefix', { type: 'string', default: '', describe: 'Image tag prefix' })
+        .option('image-tag-suffix', { type: 'string', default: '', describe: 'Image tag suffix' })
+        .option('image-tag-registry', { type: 'string', default: '', describe: 'Image tag registry' })
+        .option('changed-files-output-file', { type: 'string', describe: 'Path to write changed files', demandOption: false });
+    },
+    async (argv) => {
+      try {
+        const rules = getRules(argv.rules as string, argv['rules-file'] as string);
+        const affectedOutput = await processRules(
+          log.bind(null, argv.verbose as boolean),
+          rules,
+          argv['truncate-sha1-size'] as number,
+          argv['image-tag-registry'] as string,
+          argv['image-tag-prefix'] as string,
+          argv['image-tag-suffix'] as string,
+          argv['changed-files-output-file'] as string | undefined
+        );
+        console.info(`Affected: ${JSON.stringify(affectedOutput, null, 2)}`);
+      } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+      }
+    }
+  )
+  .help()
+  .argv;
