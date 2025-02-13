af3a32588ae9d734349748178dd042e942f4c821
Leblanc Meneses <leblancmeneses@gmail.com>
2025-02-13 12:55:54 -0600

    changes

--- apps/gcp-build-cache/src/util.ts
+++ apps/gcp-build-cache/src/util.ts
@@ -2,8 +2,10 @@ import { Storage } from '@google-cloud/storage';
 
 export async function writeCacheFileToGcs(cacheKeyPath: string) {
   const storage = new Storage();
-  const bucketName = cacheKeyPath.split('/')[0];
+
+  const bucketName = cacheKeyPath.substring(0, cacheKeyPath.indexOf('/', 5));
   const destinationFilename = cacheKeyPath.substring(bucketName.length + 1);
+
   const bucket = storage.bucket(bucketName);
   const file = bucket.file(destinationFilename);
 
